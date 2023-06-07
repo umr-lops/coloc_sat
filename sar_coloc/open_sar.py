d091d5bdaa9f4b208a89da042799b478ca30ebba
yreynaud <yann.reynaud10@gmail.com>
2023-06-07 15:02:20 +0200

    Add footprint and start/stop dates fonctionnality for multidataset and single ones

--- sar_coloc/open_sar.py
+++ sar_coloc/open_sar.py
@@ -1,7 +1,7 @@
 import os
-import pandas as pd
-from shapely import Polygon
-from .tools import call_reader
+from .tools import call_sar_meta
+from shapely.geometry import Polygon
+import numpy as np
 
 
 class OpenSar:
@@ -9,65 +9,61 @@ class OpenSar:
         self.product_path = product_path
         self.product_name = os.path.basename(self.product_path)
         self.meta = None
-        self.geoloc = None
-        self.timestamp = None
-        self._time_range = None
+        self.submeta = {}
+        self.times = None
+        self.dataset_names = None
+        self.footprints = None
         if self.product_name.endswith('.nc'):
             self.level = 2
         else:
             self.level = 1
-            self.meta = call_reader(self.product_path)
-            self.geoloc = self.meta[self.map_readers_xpath[self.satellite_name]['geoloc']]
-            self.timestamp = self.meta[self.map_readers_xpath[self.satellite_name]['timestamp']]
+            self.meta = call_sar_meta(self.product_path)
+            self.fill_dataset_names()
+            self.fill_submeta()
+            self.fill_times()
+            self.fill_footprints()
 
-    map_readers_xpath = {
-        'RS2': {
-            'geoloc': 'geolocationGrid',
-            'timestamp': 'orbitAndAttitude/timeStamp'
-        },
-        'S1': {
-            'geoloc': 'geolocationGrid',
-            'timestamp': 'orbit/time'
-        },
-        'RCM': {
-            'geoloc': 'imageReferenceAttributes/geographicInformation',
-            'timestamp': 'orbitAndAttitude/orbitInformation/timeStamp'
-        },
-    }
+    def fill_submeta(self):
+        if self.multidataset:
+            for ds_name in self.dataset_names:
+                self.submeta[ds_name] = call_sar_meta(ds_name)
+        else:
+            self.submeta[self.dataset_names[0]] = self.meta
 
-    def _get_time_range(self):
-        if self.timestamp is not None:
-            time_range = self.timestamp
-            return pd.Interval(left=pd.Timestamp(time_range.values[0]), right=pd.Timestamp(time_range.values[-1]),
-                           closed='both')
+    def fill_dataset_names(self):
+        if self.satellite_name == 'S1':
+            self.dataset_names = [ds_name for ds_name in list(self.meta.subdatasets.index)]
+        else:
+            self.dataset_names = [self.product_path]
 
-    @property
-    def time_range(self):
-        """time range as pd.Interval"""
-        if self._time_range is None:
-            self._time_range = self._get_time_range()
-        return self._time_range
+    def fill_times(self):
+        self.times = {}
+        for ds_name in self.dataset_names:
+            tmp_dic = {'start_date': self.submeta[ds_name].start_date, 'stop_date': self.submeta[ds_name].stop_date}
+            self.times[ds_name] = tmp_dic
+
+    def fill_footprints(self):
+        self.footprints = {}
+        for ds_name in self.dataset_names:
+            self.footprints[ds_name] = self.submeta[ds_name].footprint
 
     @property
-    def start_date(self):
-        """start date, as datetime.datetime"""
-        return '%s' % self.time_range.left
+    def multidataset(self):
+        if self.satellite_name == 'S1':
+            return self.meta.multidataset
+        else:
+            return False
 
-    @property
-    def stop_date(self):
-        """stort date, as datetime.datetime"""
-        return '%s' % self.time_range.right
-
-    @property
-    def footprint(self):
-        if self.geoloc is not None:
-            footprint_dict = {}
-            for ll in ['longitude', 'latitude']:
-                footprint_dict[ll] = [
-                    self.geoloc[ll].isel(line=a, pixel=x).values for a, x in [(0, 0), (0, -1), (-1, -1), (-1, 0)]
-                ]
-            corners = list(zip(footprint_dict['longitude'], footprint_dict['latitude']))
-            return Polygon(corners)
+    def datatree(self, ds_name):
+        if self.level == 1:
+            if self.satellite_name == 'RS2':
+                return self.submeta[ds_name].dt
+            elif self.satellite_name == 'S1':
+                return self.submeta[ds_name].dt
+            elif self.satellite_name == 'RCM':
+                return self.submeta[ds_name].dt
+        else:
+            raise TypeError("Datatree property only can be used for level 1 product, here level is  %s" % str(self.level))
 
     @property
     def satellite_name(self):
@@ -80,5 +76,22 @@ class OpenSar:
         else:
             raise TypeError("Unrecognized satellite name from %s" % str(self.product_name))
 
+    @property
+    def footprint(self):
+        entire_poly = Polygon()
+        for ds_name in self.dataset_names:
+            fp = self.footprints[ds_name]
+            entire_poly = entire_poly.union(fp)
+        return entire_poly
+
+    @property
+    def start_date(self):
+        start_dates = [np.datetime64(value['start_date']) for value in self.times.values()]
+        return min(start_dates)
+
+    @property
+    def stop_date(self):
+        stop_dates = [np.datetime64(value['stop_date']) for value in self.times.values()]
+        return min(stop_dates)
 
 
