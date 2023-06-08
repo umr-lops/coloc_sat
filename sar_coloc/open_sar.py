fd10037fe2ee291312bfff1cc27007ca8592dbe5
yreynaud <yann.reynaud10@gmail.com>
2023-06-08 11:17:44 +0200

    Add fonctionnality to read footprint and dates from L2 product

--- sar_coloc/open_sar.py
+++ sar_coloc/open_sar.py
@@ -1,5 +1,5 @@
 import os
-from .tools import call_sar_meta
+from .tools import call_sar_meta, open_l2, convert_str_to_polygon
 from shapely.geometry import Polygon
 import numpy as np
 
@@ -8,62 +8,75 @@ class OpenSar:
     def __init__(self, product_path):
         self.product_path = product_path
         self.product_name = os.path.basename(self.product_path)
-        self.meta = None
-        self.submeta = {}
-        self.times = None
-        self.dataset_names = None
-        self.footprints = None
-        if self.product_name.endswith('.nc'):
-            self.level = 2
-        else:
-            self.level = 1
-            self.meta = call_sar_meta(self.product_path)
+        self._l1_info = None
+        self._l2_info = None
+        if self.is_safe:
+            self._l1_info = {'meta': call_sar_meta(self.product_path),
+                             'dataset_names': [],
+                             'submeta': {},
+                             'times': {},
+                             'footprints': {}
+                             }
             self.fill_dataset_names()
             self.fill_submeta()
             self.fill_times()
             self.fill_footprints()
+        else:
+            self._l2_info = open_l2(product_path)
 
     def fill_submeta(self):
-        if self.multidataset:
-            for ds_name in self.dataset_names:
-                self.submeta[ds_name] = call_sar_meta(ds_name)
+        if self.is_safe:
+            if self.multidataset:
+                for ds_name in self._l1_info['dataset_names']:
+                    self._l1_info['submeta'][ds_name] = call_sar_meta(ds_name)
+            else:
+                self._l1_info['submeta'][self._l1_info['dataset_names'][0]] = self._l1_info['meta']
         else:
-            self.submeta[self.dataset_names[0]] = self.meta
+            raise TypeError("fill_submeta property only can be used for level 1 product")
 
     def fill_dataset_names(self):
-        if self.satellite_name == 'S1':
-            self.dataset_names = [ds_name for ds_name in list(self.meta.subdatasets.index)]
+        if self.is_safe:
+            if self.satellite_name == 'S1':
+                self._l1_info['dataset_names'] = [ds_name for ds_name in list(self._l1_info['meta'].subdatasets.index)]
+            else:
+                self._l1_info['dataset_names'] = [self.product_path]
         else:
-            self.dataset_names = [self.product_path]
+            raise TypeError("fill_dataset_names property only can be used for level 1 product")
 
     def fill_times(self):
-        self.times = {}
-        for ds_name in self.dataset_names:
-            tmp_dic = {'start_date': self.submeta[ds_name].start_date, 'stop_date': self.submeta[ds_name].stop_date}
-            self.times[ds_name] = tmp_dic
+        if self.is_safe:
+            for ds_name in self._l1_info['dataset_names']:
+                tmp_dic = {
+                    'start_date': self._l1_info['submeta'][ds_name].start_date,
+                    'stop_date': self._l1_info['submeta'][ds_name].stop_date}
+                self._l1_info['times'][ds_name] = tmp_dic
+        else:
+            raise TypeError("fill_times property only can be used for level 1 product")
 
     def fill_footprints(self):
-        self.footprints = {}
-        for ds_name in self.dataset_names:
-            self.footprints[ds_name] = self.submeta[ds_name].footprint
+        if self.is_safe:
+            for ds_name in self._l1_info['dataset_names']:
+                self._l1_info['footprints'][ds_name] = self._l1_info['submeta'][ds_name].footprint
+        else:
+            raise TypeError("fill_footprints property only can be used for level 1 product")
 
     @property
     def multidataset(self):
         if self.satellite_name == 'S1':
-            return self.meta.multidataset
+            return self._l1_info['meta'].multidataset
         else:
             return False
 
     def datatree(self, ds_name):
-        if self.level == 1:
+        if self.is_safe:
             if self.satellite_name == 'RS2':
-                return self.submeta[ds_name].dt
+                return self._l1_info['submeta'][ds_name].dt
             elif self.satellite_name == 'S1':
-                return self.submeta[ds_name].dt
+                return self._l1_info['submeta'][ds_name].dt
             elif self.satellite_name == 'RCM':
-                return self.submeta[ds_name].dt
+                return self._l1_info['submeta'][ds_name].dt
         else:
-            raise TypeError("Datatree property only can be used for level 1 product, here level is  %s" % str(self.level))
+            raise TypeError("datatree property only can be used for level 1 product")
 
     @property
     def satellite_name(self):
@@ -78,20 +91,36 @@ class OpenSar:
 
     @property
     def footprint(self):
-        entire_poly = Polygon()
-        for ds_name in self.dataset_names:
-            fp = self.footprints[ds_name]
-            entire_poly = entire_poly.union(fp)
-        return entire_poly
+        if self.is_safe:
+            entire_poly = Polygon()
+            for ds_name in self._l1_info['dataset_names']:
+                fp = self._l1_info['footprints'][ds_name]
+                entire_poly = entire_poly.union(fp)
+            return entire_poly
+        else:
+            return convert_str_to_polygon(self._l2_info.attrs['footprint'])
 
     @property
     def start_date(self):
-        start_dates = [np.datetime64(value['start_date']) for value in self.times.values()]
-        return min(start_dates)
+        if self.is_safe:
+            start_dates = [np.datetime64(value['start_date']) for value in self._l1_info['times'].values()]
+            return min(start_dates)
+        else:
+            return np.datetime64(self._l2_info.attrs['firstMeasurementTime'])
 
     @property
     def stop_date(self):
-        stop_dates = [np.datetime64(value['stop_date']) for value in self.times.values()]
-        return min(stop_dates)
+        if self.is_safe:
+            stop_dates = [np.datetime64(value['stop_date']) for value in self._l1_info['times'].values()]
+            return min(stop_dates)
+        else:
+            return np.datetime64(self._l2_info.attrs['lastMeasurementTime'])
+
+    @property
+    def is_safe(self):
+        if self.product_name.endswith('.nc'):
+            return False
+        else:
+            return True
 
 
