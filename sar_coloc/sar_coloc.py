51d5f8e5699accd2a027dd7839e7d0785a8b10d1
yreynaud <yann.reynaud10@gmail.com>
2023-06-14 16:58:16 +0200

    Make more general the opening of comparison acquisition

--- sar_coloc/sar_coloc.py
+++ sar_coloc/sar_coloc.py
@@ -1,21 +1,17 @@
 """Main module."""
-from .tools import get_all_comparison_files
+from .tools import get_all_comparison_files, get_acquisition_root_paths, call_open_class
 from .open_sar import OpenSar
-from .open_smos import OpenSmos
 import numpy as np
 
 
 class SarColoc:
     def __init__(self, sar_id, db_name='SMOS', delta_time=3):
-        roots = {
-            'SMOS': ['/home/ref-smoswind-public/data/v3.0/l3/data/reprocessing',
-                     '/home/ref-smoswind-public/data/v3.0/l3/data/nrt'],
-        }
+        roots = get_acquisition_root_paths(db_name)
         self.db_name = db_name
         self.sar = OpenSar(sar_id)
         self.delta_time = delta_time
         self.comparison_files = []
-        self.comparison_files += get_all_comparison_files(roots[db_name], self.start_date, self.stop_date,
+        self.comparison_files += get_all_comparison_files(roots, self.start_date, self.stop_date,
                                                           db_name=self.db_name)
         self.common_footprints = None
         self.fill_footprints()
@@ -31,7 +27,7 @@ class SarColoc:
     def fill_footprints(self):
         _footprints = {}
         for file in self.comparison_files:
-            opened_file = OpenSmos(file)
+            opened_file = call_open_class(file, self.db_name)
             if self.sar.footprint.intersects(opened_file.footprint(self.start_date, self.stop_date)):
                 _footprints[file] = self.sar.footprint\
                     .intersection(opened_file.footprint(self.start_date, self.stop_date))
