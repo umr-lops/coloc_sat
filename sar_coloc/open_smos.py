7abcaaed4b5b7ab6265554feb50aae127e38ef11
yreynaud <yann.reynaud10@gmail.com>
2023-06-19 13:31:51 +0200

    Add geographical intersection and consider wind speed

--- sar_coloc/open_smos.py
+++ sar_coloc/open_smos.py
@@ -1,17 +1,21 @@
+import rasterio
 from shapely import Polygon
 
 from .tools import open_smos_file
 import os
 import numpy as np
-import xarray as xr
 from shapely.geometry import MultiPoint
+
+
+def extract_wind_speed(smos_dataset):
+    return smos_dataset.where(~np.isnan(smos_dataset.wind_speed), drop=True)
 
 
 class OpenSmos:
     def __init__(self, product_path):
         self.product_path = product_path
         self.product_name = os.path.basename(self.product_path)
-        self.dataset = open_smos_file(product_path)
+        self.dataset = open_smos_file(product_path).squeeze()
 
     @property
     def start_date(self):
@@ -23,7 +27,7 @@ class OpenSmos:
         numpy.datetime64
             Start time
         """
-        return min(np.unique(self.times.squeeze()))
+        return min(np.unique(self.dataset.measurement_time))
 
     @property
     def stop_date(self):
@@ -35,9 +39,9 @@ class OpenSmos:
         numpy.datetime64
             Stop time
         """
-        return max(np.unique(self.times.squeeze()))
+        return max(np.unique(self.dataset.measurement_time))
 
-    def footprint(self, start_date=None, stop_date=None):
+    def footprint(self, sar_polygon, start_date=None, stop_date=None):
         """
         Get the footprint between 2 times. If no one specified, get the footprint between the start and stop
         acquisition times
@@ -55,50 +59,76 @@ class OpenSmos:
             Resulting footprint
         """
         entire_poly = Polygon()
-        times = self.extract_times(start_date, stop_date)
+        cropped_ds = self.spatial_geographic_intersection(sar_polygon, start_date, stop_date)
+        wind = extract_wind_speed(cropped_ds)
         # if the footprint cross the meridian, we split the footprint in 2 ones
-        if any(times.lon < 0):
+        if any(wind.lon < 0):
             conditions = [
-                times.lon < 0,
-                times.lon >= 0
+                wind.lon < 0,
+                wind.lon >= 0
             ]
             for condition in conditions:
-                conditioned_time = times.where(condition, drop=True)
-                stacked_times = conditioned_time.stack({"cells": ["lon", "lat"]}).dropna(dim="cells")
-                mpt = MultiPoint(stacked_times.cells.data)
+                conditioned_wind = wind.where(condition, drop=True)
+                stacked_wind = conditioned_wind.stack({"cells": ["lon", "lat"]}).dropna(dim="cells")
+                mpt = MultiPoint(stacked_wind.cells.data)
                 entire_poly = entire_poly.union(mpt.convex_hull)
         else:
-            stacked_times = times.stack({"cells": ["lon", "lat"]}).dropna(dim="cells")
-            mpt = MultiPoint(stacked_times.cells.data)
+            stacked_wind = wind.stack({"cells": ["lon", "lat"]}).dropna(dim="cells")
+            mpt = MultiPoint(stacked_wind.cells.data)
             entire_poly = mpt.convex_hull
         return entire_poly
 
-    @property
-    def cross_antemeridian(self):
+    def cross_antemeridian(self, dataset):
         """True if footprint cross antemeridian"""
-        times = self.dataset.measurement_time.squeeze()
-        return ((np.max(times.lon) - np.min(
-            times.lon)) > 180).item()
+        return ((np.max(dataset.lon) - np.min(
+            dataset.lon)) > 180).item()
 
     @property
-    def times(self):
+    def corrected_dataset(self):
         """
-        Get acquisition times depending on latitude and longitude. Apply correction if needed when it crosses antemeridian.
+        Get acquisition dataset depending on latitude and longitude. Apply correction if needed when it crosses antemeridian.
         Longitude values are ranging between -180 and 180 degrees.
 
         Returns
         -------
         xarray.Dataset
-            Acquisition times depending on longitude and latitude.
+            Acquisition dataset depending on longitude and latitude.
         """
-        times = self.dataset.measurement_time.squeeze()
-        if self.cross_antemeridian:
-            times['lon'] = times.lon % 360
-        return times.assign_coords({'lon': xr.where(times.lon > 180, times.lon - 360, times.lon)})
+        dataset = self.dataset
+        """if self.cross_antemeridian(dataset):
+            dataset['lon'] = dataset.lon % 360
+        return dataset.assign_coords({'lon': xr.where(dataset.lon > 180, dataset.lon - 360, dataset.lon)})"""
+        dataset = dataset.assign_coords(lon=(((dataset.lon + 180) % 360) - 180)).sortby('lon')
+        return dataset
 
-    def extract_times(self, start_date=None, stop_date=None):
+    def rasterize_polygon(self, polygon):
+        min_bounds = (min(np.unique(self.corrected_dataset.lon)), min(np.unique(self.corrected_dataset.lat)))
+        lon_res = float(self.corrected_dataset.attrs['geospatial_lon_resolution'])
+        lat_res = float(self.corrected_dataset.attrs['geospatial_lat_resolution'])
+        out_shape = [len(self.corrected_dataset.lat), len(self.corrected_dataset.lon)]
+        transform = rasterio.Affine.translation(min_bounds[0], min_bounds[1]) * rasterio.Affine.scale(lon_res, lat_res)
+        return rasterio.features.rasterize(shapes=[polygon], out_shape=out_shape, transform=transform)
+
+    def geographic_intersection(self, sar_polygon=None):
+        if sar_polygon is None:
+            return self.corrected_dataset
+        else:
+            rasterized = self.rasterize_polygon(sar_polygon)
+
+            ds = self.corrected_dataset.where(rasterized)
+
+            ds = ds.dropna('lon', how='all')
+            ds = ds.dropna('lat', how='all')
+            return ds
+
+    def spatial_geographic_intersection(self, sar_polygon=None, start_date=None, stop_date=None):
+        ds = self.geographic_intersection(sar_polygon)
+        ds = self.extract_times_dataset(ds, start_date, stop_date)
+        return ds
+
+    def extract_times_dataset(self, smos_dataset, start_date=None, stop_date=None):
         """
-        Extract a sub-dataset from `OpenSmos.times` to get a time dataset within 2 bounds (dates). If one of th bound
+        Extract a sub-dataset from `OpenSmos.dataset` to get a time dataset within 2 bounds (dates). If one of th bound
         exceeds the acquisition extremum times, so the acquisition Start and/ or Stop dates are chosen.
         Parameters
         ----------
@@ -116,6 +146,6 @@ class OpenSmos:
             start_date = self.start_date
         if stop_date is None:
             stop_date = self.stop_date
-        return self.times.where(lambda arr: (arr >= start_date) & (arr <= stop_date), drop=True)
-
-
+        extracted_ds = smos_dataset.where((smos_dataset.measurement_time >= start_date) &
+                                          (smos_dataset.measurement_time <= stop_date), drop=True)
+        return extracted_ds
