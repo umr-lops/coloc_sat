02e24df4a62e554e0df550abdbc4d65dbfda53db
yreynaud <yann.reynaud10@gmail.com>
2023-06-28 16:28:56 +0200

    Add access to principal classes

--- sar_coloc/__init__.py
+++ sar_coloc/__init__.py
@@ -3,5 +3,12 @@
 __author__ = """Yann Reynaud"""
 __email__ = 'yann.reynaud.2@ifremer.fr'
 __version__ = '0.1.0'
+__all__ = ['OpenSar', 'OpenHy', 'OpenEra', 'OpenSmos']
 
+from .open_sar import OpenSar
+from .open_era import OpenEra
+from .sar_coloc import SarColoc
+from .open_hy import OpenHy
+from .open_smos import OpenSmos
 from . import *
+
