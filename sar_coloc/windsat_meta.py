f654b865838d7bb3c7d736e161d3cdc2c1fe5747
yreynaud <yann.reynaud10@gmail.com>
2023-07-05 11:37:04 +0200

    Replace mingmt by time variable in dataset

--- sar_coloc/windsat_meta.py
+++ sar_coloc/windsat_meta.py
@@ -1,5 +1,7 @@
 import os
 import numpy as np
+import xarray as xr
+from datetime import datetime, timedelta
 
 from sar_coloc.tools import correct_dataset
 from .windsat_daily_v7 import WindSatDaily, to_xarray_dataset
@@ -11,6 +13,7 @@ class GetWindSatMeta:
         self.product_name = os.path.basename(self.product_path)
         self.dataset = to_xarray_dataset(WindSatDaily(product_path, np.nan))
         self.set_dataset(correct_dataset(self.dataset, self.longitude_name))
+        #self.set_dataset(self.convert_mingmt())
 
     @property
     def longitude_name(self):
@@ -46,7 +49,7 @@ class GetWindSatMeta:
         str
             time name
         """
-        return 'mingmt'
+        return 'time'
 
     def set_dataset(self, dataset):
         """
@@ -58,5 +61,50 @@ class GetWindSatMeta:
             new Dataset
         """
         self.dataset = dataset
+
+    @property
+    def day_date(self):
+        """
+        Get day date from the product name as a datetime
+
+        Returns
+        -------
+        datetime.datetime
+        Day date of the product
+        """
+        str_date = self.product_name.split('_')[1].split('v')[0]
+        return datetime.strptime(str_date, '%Y%m%d')
+
+    def convert_mingmt_ufunc(self, mingmt):
+        """
+        Convert a WindSat time value to the numpy.datetime64 format.
+        Parameters:
+            mingmt (int): WindSat time value in the minutes since midnight GMT format.
+        Returns:
+            (numpy.datetime64): WindSat time value reformated.
+        """
+        if np.isfinite(mingmt):
+            hours = int(mingmt // 60)
+            minutes = int(mingmt % 60)
+            day_date = self.day_date
+            if hours == 24:
+                hours = 0
+                day_date += timedelta(days=1)
+            return np.datetime64(day_date.replace(hour=hours, minute=minutes, second=0))
+        else:
+            return np.datetime64("NaT")
+
+    def convert_mingmt(self):
+        """
+        Convert a WindSat time array to the numpy.datetime64 format.
+        Returns:
+            (xarray.Dataset): Co-located WindSat dataset with time reformated.
+        """
+        ds = self.dataset
+        ds[self.time_name] = xr.apply_ufunc(self.convert_mingmt_ufunc, ds['mingmt'],
+                                    input_core_dims=[[]],
+                                    dask="parallelized", vectorize=True)
+        return ds.drop_vars(['mingmt'])
+
 
 
