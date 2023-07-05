0d45c909add35dd01e3bec2a1641ce44c618e67a
yreynaud <yann.reynaud10@gmail.com>
2023-07-05 16:22:23 +0200

    Add segmentation notion + start/stop dates + acquisition type

--- sar_coloc/windsat_meta.py
+++ sar_coloc/windsat_meta.py
@@ -11,7 +11,7 @@ class GetWindSatMeta:
     def __init__(self, product_path, listing=True):
         self.product_path = product_path
         self.product_name = os.path.basename(self.product_path)
-        self.dataset = to_xarray_dataset(WindSatDaily(product_path, np.nan))
+        self.dataset = to_xarray_dataset(WindSatDaily(product_path, np.nan)).load()
         self.set_dataset(correct_dataset(self.dataset, self.longitude_name))
         #self.set_dataset(self.convert_mingmt())
 
@@ -105,6 +105,71 @@ class GetWindSatMeta:
                                     input_core_dims=[[]],
                                     dask="parallelized", vectorize=True)
         return ds.drop_vars(['mingmt'])
+
+    @property
+    def acquisition_type(self):
+        """
+        Gives the acquisition type (swath, truncated_swath,daily_regular_grid, model_regular_grid)
+
+        Returns
+        -------
+        str
+            acquisition type
+
+        """
+        return 'daily_regular_grid'
+
+    @property
+    def start_date(self):
+        """
+        Start acquisition time
+
+        Returns
+        -------
+        numpy.datetime64
+            Start time
+        """
+        return min(np.unique(self.dataset[self.time_name]))
+
+    @property
+    def stop_date(self):
+        """
+        Stop acquisition time
+
+        Returns
+        -------
+        numpy.datetime64
+            Stop time
+        """
+        return max(np.unique(self.dataset[self.time_name]))
+
+    @property
+    def orbit_segment_name(self):
+        """
+        Gives the name of the variable for orbit segmentation in dataset (Ascending / Descending). If value is None,
+        so the orbit hasn't orbited segmentation
+
+        Returns
+        -------
+        str | None
+            Orbit segmentation variable name in the dataset. None if there isn't one.
+        """
+        return 'orbit_segment'
+
+    @property
+    def has_orbited_segmentation(self):
+        """
+        True if there is orbit segmentation in the dataset
+
+        Returns
+        -------
+        bool
+            Presence or not of an orbit segmentation
+        """
+        if self.orbit_segment_name is not None:
+            return True
+        else:
+            return False
 
 
 
