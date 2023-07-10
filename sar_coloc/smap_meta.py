fb29e79413f85aedc9599d3607110d08a4301faf
yreynaud <yann.reynaud10@gmail.com>
2023-07-10 16:25:12 +0200

    Correct longitude and latitude names

--- sar_coloc/smap_meta.py
+++ sar_coloc/smap_meta.py
@@ -27,7 +27,7 @@ class GetSmapMeta:
         str
             longitude name
         """
-        return 'longitude'
+        return 'lon'
 
     @property
     def latitude_name(self):
@@ -39,7 +39,7 @@ class GetSmapMeta:
         str
             latitude name
         """
-        return 'latitude'
+        return 'lat'
 
     @property
     def time_name(self):
