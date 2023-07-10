3a19c33c4c96b3331e82be88d31c3ea3c1823358
yreynaud <yann.reynaud10@gmail.com>
2023-07-10 15:41:57 +0200

    Add GetSmapMeta

--- sar_coloc/__init__.py
+++ sar_coloc/__init__.py
@@ -3,7 +3,7 @@
 __author__ = """Yann Reynaud"""
 __email__ = 'yann.reynaud.2@ifremer.fr'
 __version__ = '0.1.0'
-__all__ = ['GetSarMeta', 'GetEra5Meta', 'GetHy2Meta', 'GetSmosMeta', 'FindColoc', 'GetWindSatMeta']
+__all__ = ['GetSarMeta', 'GetEra5Meta', 'GetHy2Meta', 'GetSmosMeta', 'FindColoc', 'GetWindSatMeta', 'GetSmapMeta']
 
 from .sar_meta import GetSarMeta
 from .era5_meta import GetEra5Meta
@@ -11,5 +11,6 @@ from .find_coloc import FindColoc
 from .hy2_meta import GetHy2Meta
 from .smos_meta import GetSmosMeta
 from .windsat_meta import GetWindSatMeta
+from .smap_meta import GetSmapMeta
 from . import *
 
