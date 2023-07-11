a94fc1f4220146ca301e5e4e0a73de93bc52314a
yreynaud <yann.reynaud10@gmail.com>
2023-07-11 10:04:37 +0200

    Rename FindColoc --> FindFileColoc

--- sar_coloc/__init__.py
+++ sar_coloc/__init__.py
@@ -3,11 +3,11 @@
 __author__ = """Yann Reynaud"""
 __email__ = 'yann.reynaud.2@ifremer.fr'
 __version__ = '0.1.0'
-__all__ = ['GetSarMeta', 'GetEra5Meta', 'GetHy2Meta', 'GetSmosMeta', 'FindColoc', 'GetWindSatMeta', 'GetSmapMeta']
+__all__ = ['GetSarMeta', 'GetEra5Meta', 'GetHy2Meta', 'GetSmosMeta', 'FindFileColoc', 'GetWindSatMeta', 'GetSmapMeta']
 
 from .sar_meta import GetSarMeta
 from .era5_meta import GetEra5Meta
-from .find_coloc import FindColoc
+from .find_file_coloc import FindFileColoc
 from .hy2_meta import GetHy2Meta
 from .smos_meta import GetSmosMeta
 from .windsat_meta import GetWindSatMeta
---
+++ sar_coloc/find_file_coloc.py
@@ -0,0 +1,82 @@
+"""Main module."""
+from .tools import get_all_comparison_files, call_meta_class
+from .intersection_tools import has_intersection
+import numpy as np
+
+
+class FindFileColoc:
+    # Choices:
+    # - Don't always use footprint for all intersection types (because sometimes it needs more processing than it
+    # is necessary for a listing)
+    # - Use a function to fill co-located files instead of using a property, so that it is computed once.
+    def __init__(self, product_id, db_name='SMOS', level=None, delta_time=60):
+        self.product_id = product_id
+        self.db_name = db_name
+        self.level = level
+        self.product = call_meta_class(product_id)
+        self.delta_time = np.timedelta64(delta_time, 'm')
+        self.comparison_files = self.get_comparison_files
+        self.common_footprints = None
+        #self.fill_footprints()
+        self.colocated_files = None
+        self.fill_colocated_files()
+
+    @property
+    def start_date(self):
+        return self.product.start_date - np.timedelta64(self.delta_time, 'm')
+
+    @property
+    def stop_date(self):
+        return self.product.stop_date + np.timedelta64(self.delta_time, 'm')
+
+    def fill_footprints(self):
+        _footprints = {}
+        for file in self.comparison_files:
+            opened_file = call_meta_class(file)
+            if self.product.footprint.intersects(
+                    opened_file.footprint(self.product.footprint, self.start_date, self.stop_date)):
+                _footprints[file] = self.product.footprint \
+                    .intersection(opened_file.footprint(self.product.footprint, self.start_date, self.stop_date))
+            else:
+                _footprints[file] = None
+        # if no common values, let the footprint with the value None
+        if all(value is None for value in _footprints.values()):
+            pass
+        else:
+            self.common_footprints = _footprints
+
+    def fill_colocated_files(self):
+        _colocated_files = []
+        for file in self.comparison_files:
+            try:
+                opened_file = call_meta_class(file)
+                if has_intersection(self.product, opened_file, delta_time=self.delta_time):
+                    _colocated_files.append(file)
+            except FileNotFoundError:
+                pass
+            #print(f"The file {file} has been treated. \n Progression : {(self.comparison_files.index(file) +1 ) * 100 / len(self.comparison_files)}\n ########")
+        if len(_colocated_files) > 0:
+            self.colocated_files = _colocated_files
+
+    @property
+    def has_coloc(self):
+        if self.colocated_files is None:
+            return False
+        else:
+            return True
+
+    @property
+    def get_comparison_files(self):
+        """
+        Get all the files from the specified database that match with the start and stop dates
+
+        Returns
+        -------
+        list
+            Comparison files
+        """
+        all_comparison_files = get_all_comparison_files(self.start_date, self.stop_date, db_name=self.db_name,
+                                                        level=self.level)
+        if self.product_id in all_comparison_files:
+            all_comparison_files.remove(self.product_id)
+        return all_comparison_files
