ff2d716e2bc2f8deaa9b4f0475ad3c8f8c115290
yreynaud <yann.reynaud10@gmail.com>
2023-07-11 16:40:05 +0200

    Add find_coloc

--- sar_coloc/__init__.py
+++ sar_coloc/__init__.py
@@ -3,7 +3,8 @@
 __author__ = """Yann Reynaud"""
 __email__ = 'yann.reynaud.2@ifremer.fr'
 __version__ = '0.1.0'
-__all__ = ['GetSarMeta', 'GetEra5Meta', 'GetHy2Meta', 'GetSmosMeta', 'FindFileColoc', 'GetWindSatMeta', 'GetSmapMeta']
+__all__ = ['GetSarMeta', 'GetEra5Meta', 'GetHy2Meta', 'GetSmosMeta', 'FindFileColoc', 'GetWindSatMeta',
+           'GetSmapMeta', 'FindColoc']
 
 from .sar_meta import GetSarMeta
 from .era5_meta import GetEra5Meta
@@ -12,5 +13,6 @@ from .hy2_meta import GetHy2Meta
 from .smos_meta import GetSmosMeta
 from .windsat_meta import GetWindSatMeta
 from .smap_meta import GetSmapMeta
+from .find_coloc import FindColoc
 from . import *
 
