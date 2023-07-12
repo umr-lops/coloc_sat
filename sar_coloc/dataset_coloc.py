bd0b7f40d300b65fbad943e2b7f97d80254cc113
yreynaud <yann.reynaud10@gmail.com>
2023-07-12 13:12:42 +0200

    Rename FindFileColoc --> FindProductColoc

--- sar_coloc/dataset_coloc.py
+++ sar_coloc/dataset_coloc.py
@@ -1,11 +1,11 @@
 from .tools import get_all_comparison_files
-from .find_file_coloc import FindFileColoc
+from .find_product_coloc import FindProductColoc
 
 import os
 import numpy as np
 
 
-class FindColoc:
+class ColocOnDataset:
     def __init__(self, ds_name1='RCM', ds_name2='SMOS', destination='', level=None, delta_time=60, listing=True):
         self.ds_name1 = ds_name1
         self.ds_name2 = ds_name2
@@ -20,12 +20,11 @@ class FindColoc:
 
     def run_colocation_search(self):
         for product in self.products:
-            coloc_finder = FindFileColoc(product_id=product, ds_name=self.ds_name2, level=self.level,
-                                         delta_time=self.delta_time, listing=self.listing)
+            coloc_finder = FindProductColoc(product_id=product, ds_name=self.ds_name2, level=self.level,
+                                            delta_time=self.delta_time, listing=self.listing)
             self.colocated_files[product] = coloc_finder.colocated_files
 
     def create_listing_file(self, prefix='coloc_listing'):
         basename = f"{prefix}_{self.ds_name1}_{self.ds_name2}.npy"
         full_path = os.path.join(self.destination, basename)
         np.save(full_path, self.colocated_files)
-
