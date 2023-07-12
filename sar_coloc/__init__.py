3b56d5485555627d5a7341f7aef0fb82c03802ee
yreynaud <yann.reynaud10@gmail.com>
2023-07-12 13:09:47 +0200

    Rename find_coloc --> dataset_coloc and find_file_coloc --> find_product_coloc

--- sar_coloc/__init__.py
+++ sar_coloc/__init__.py
@@ -3,16 +3,16 @@
 __author__ = """Yann Reynaud"""
 __email__ = 'yann.reynaud.2@ifremer.fr'
 __version__ = '0.1.0'
-__all__ = ['GetSarMeta', 'GetEra5Meta', 'GetHy2Meta', 'GetSmosMeta', 'FindFileColoc', 'GetWindSatMeta',
-           'GetSmapMeta', 'FindColoc']
+__all__ = ['GetSarMeta', 'GetEra5Meta', 'GetHy2Meta', 'GetSmosMeta', 'FindProductColoc', 'GetWindSatMeta',
+           'GetSmapMeta', 'ColocOnDataset']
 
 from .sar_meta import GetSarMeta
 from .era5_meta import GetEra5Meta
-from .find_file_coloc import FindFileColoc
+from .find_product_coloc import FindProductColoc
 from .hy2_meta import GetHy2Meta
 from .smos_meta import GetSmosMeta
 from .windsat_meta import GetWindSatMeta
 from .smap_meta import GetSmapMeta
-from .find_coloc import FindColoc
+from .dataset_coloc import ColocOnDataset
 from . import *
 
