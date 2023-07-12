82e65b189a08f9a55edd0bf070bc4fca6734b322
yreynaud <yann.reynaud10@gmail.com>
2023-07-12 14:58:44 +0200

    Add ProductIntersection

--- sar_coloc/__init__.py
+++ sar_coloc/__init__.py
@@ -4,7 +4,7 @@ __author__ = """Yann Reynaud"""
 __email__ = 'yann.reynaud.2@ifremer.fr'
 __version__ = '0.1.0'
 __all__ = ['GetSarMeta', 'GetEra5Meta', 'GetHy2Meta', 'GetSmosMeta', 'FindProductColoc', 'GetWindSatMeta',
-           'GetSmapMeta', 'ColocOnDataset']
+           'GetSmapMeta', 'ColocOnDataset', 'ProductIntersection']
 
 from .sar_meta import GetSarMeta
 from .era5_meta import GetEra5Meta
@@ -14,5 +14,6 @@ from .smos_meta import GetSmosMeta
 from .windsat_meta import GetWindSatMeta
 from .smap_meta import GetSmapMeta
 from .dataset_coloc import ColocOnDataset
+from .intersection import ProductIntersection
 from . import *
 
