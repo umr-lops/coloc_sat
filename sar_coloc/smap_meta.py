d597b5a8498f42149e99b93142a91b56870edc35
yreynaud <yann.reynaud10@gmail.com>
2023-08-04 15:21:43 +0200

    Add missing reference attribute to dataset + necessary methods for colocation product creation

--- sar_coloc/smap_meta.py
+++ sar_coloc/smap_meta.py
@@ -3,7 +3,7 @@ import numpy as np
 from datetime import datetime
 import xarray as xr
 
-from .tools import open_nc, convert_mingmt, correct_dataset
+from .tools import open_nc, convert_mingmt, correct_dataset, common_var_names
 
 
 class GetSmapMeta:
@@ -11,6 +11,7 @@ class GetSmapMeta:
         self.product_path = product_path
         self.product_name = os.path.basename(self.product_path)
         self.dataset = open_nc(product_path).load()
+        self.set_dataset(self.add_source_reference_attribute(ds=self.dataset))
         self.set_dataset(correct_dataset(self.dataset, self.longitude_name))
         self.set_dataset(convert_mingmt(self))
         # Modify orbit values by ascending and descending to be more significant
@@ -167,6 +168,79 @@ class GetSmapMeta:
         """
         return "SMAP"
 
+    def rename_vars_in_coloc(self, dataset=None):
+        """
+        Rename variables from a dataset to homogenize the co-location product. If no dataset is explicit, so it is this
+        of `self.dataset` which is used.
+
+        Parameters
+        ----------
+        dataset: xarray.Dataset | None
+            Dataset on which common vars must be renamed
+
+        Returns
+        -------
+        xarray.Dataset
+            Dataset with homogene variable names
+        """
+        if dataset is None:
+            dataset = self.dataset
+        # map the variable names in the dataset with the keys in common vars
+        mapper = {
+            self.wind_name: 'wind_speed',
+        }
+        for var in dataset.variables:
+            if var in mapper.keys():
+                key_in_common_vars = mapper[var]
+                dataset = dataset.rename_vars({var: common_var_names[key_in_common_vars]})
+        return dataset
+
+    @property
+    def unecessary_vars_in_coloc_product(self):
+        """
+        Get unecessary variables in co-location product
+
+        Returns
+        -------
+        list[str]
+            Unecessary variables in co-location product
+        """
+        return [self.orbit_segment_name, self.time_name]
+
+    @property
+    def necessary_attrs_in_coloc_product(self):
+        """
+        Get necessary dataset attributes in co-location product
+
+        Returns
+        -------
+        list[str]
+            Necessary dataset attributes in co-location product
+        """
+        return ['Conventions', 'title', 'institution', 'grid_mapping', 'version', 'sourceReference']
+
+    def rename_attrs_in_coloc_product(self, attr):
+        """
+        Get the new name of an attribute in co-location products from an original attribute
+
+        Parameters
+        ----------
+        attr: str
+            Attribute from the satellite dataset that needs to be renames for the co-location product.
+
+        Returns
+        -------
+        str
+            New attribute's name from the satellite dataset.
+        """
+        mapper = {
+            'version': 'sourceProductVersion',
+        }
+        if attr in mapper.keys():
+            return mapper[attr]
+        else:
+            return attr
+
     @property
     def wind_name(self):
         """
@@ -179,4 +253,54 @@ class GetSmapMeta:
 
         """
         return 'wind'
+
+    def add_source_reference_attribute(self, ds=None, attr_name='reference'):
+        """
+        Add the source reference attribute in a SMAP dataset. The name given to this attribute is chosen with the
+        argument  `attr_name`
+
+        Parameters
+        ----------
+        ds: xarray.Dataset | None
+            Dataset that contains the SMAP information, in which the source reference attribute must be added.
+        attr_name: str
+            Name chosen for the reference attribute.
+
+        Returns
+        -------
+        xarray.Dataset
+            Dataset that contains the source reference attribute
+        """
+        content = {
+            'authors': None,
+            'year': None,
+            'product': None,
+            'institution': None,
+            'weblink': None,
+        }
+        prefix = 'dataset_citation_'
+        if ds is None:
+            ds = self.dataset
+
+        def is_empty():
+            for k, val in content.items():
+                if val is not None:
+                    return False
+                else:
+                    content[k] = ''
+            return True
+
+        for key in content.keys():
+            try:
+                content[key] = ds.attrs[f"{prefix}{key}"]
+            except KeyError:
+                pass
+        if is_empty():
+            ds.attrs[attr_name] = ''
+        else:
+            version = 'Version 01.0. [NRT or FINAL].'
+            ds.attrs[attr_name] = f"{content['authors']}, {content['year']}: {content['product']}, {version}, " + \
+                                  f"{content['institution']}. {content['weblink']}"
+        return ds
+
 
