c76098aa1201f7b3af1d394eab5633a3df29364a
yreynaud <yann.reynaud10@gmail.com>
2023-08-21 11:37:04 +0200

    Add reformat fonction to only keep one resolution for lon/lat + add functions to generate the coloc product

--- sar_coloc/era5_meta.py
+++ sar_coloc/era5_meta.py
@@ -3,14 +3,21 @@ from .tools import open_nc, correct_dataset, parse_date
 
 
 class GetEra5Meta:
-    def __init__(self, product_path, listing=True):
+    def __init__(self, product_path, product_generation=False):
         self.product_path = product_path
         self.product_name = os.path.basename(self.product_path)
+        self.product_generation = product_generation
         self._dataset = None
-        if not listing:
+        # These attributes will be defined when the dataset will be reformatted for the coloc product generation
+        # (see self.reformat_meta)
+        self._longitude_name = None
+        self._latitude_name = None
+
+        if self.product_generation:
             self._dataset = open_nc(product_path).load()
-            self.dataset = correct_dataset(self.dataset, lon_name=self.longitude_name(0.25))
-            self.dataset = correct_dataset(self.dataset, lon_name=self.longitude_name(0.5))
+            self.dataset = correct_dataset(self.dataset, lon_name=self.longitude_name_res(0.25))
+            self.dataset = correct_dataset(self.dataset, lon_name=self.longitude_name_res(0.5))
+            self.reformat_meta()
 
     @property
     def start_date(self):
@@ -40,7 +47,7 @@ class GetEra5Meta:
         str_time = self.product_path.split('_')[-1].split('.')[0] + '230000'
         return parse_date(str_time)
 
-    def longitude_name(self, resolution):
+    def longitude_name_res(self, resolution):
         """
         Get the name of the longitude variable in the dataset. For ERA 5, two longitude variable exist :
         one with a resolution of 0.25; and one with a resolution of 0.5
@@ -64,7 +71,7 @@ class GetEra5Meta:
         else:
             raise ValueError(f"{name} wasn't found in the dataset. Please verify the resolution is correct")
 
-    def latitude_name(self, resolution):
+    def latitude_name_res(self, resolution):
         """
         Get the name of the latitude variable in the dataset. For ERA 5, two latitude variable exist :
         one with a resolution of 0.25; and one with a resolution of 0.5
@@ -176,3 +183,121 @@ class GetEra5Meta:
             Mission name (ex: SMOS, S1, RS2, RCM, SMAP, HY2, ERA5)
         """
         return "ERA5"
+
+    @property
+    def wind_name(self):
+        """
+        Name of an important wind variable in the dataset
+
+        Returns
+        -------
+        str
+            Wind variable name
+
+        """
+        return 'v100'
+
+    @property
+    def longitude_name(self):
+        """
+        Get the name of the longitude variable in the dataset
+
+        Returns
+        -------
+        str
+            longitude name
+        """
+        if self._longitude_name is None:
+            return ''
+        else:
+            return self._longitude_name
+
+    @property
+    def latitude_name(self):
+        """
+        Get the name of the latitude variable in the dataset
+
+        Returns
+        -------
+        str
+            longitude name
+        """
+        if self._latitude_name is None:
+            return ''
+        else:
+            return self._latitude_name
+
+    @longitude_name.setter
+    def longitude_name(self, value):
+        self._longitude_name = value
+
+    @latitude_name.setter
+    def latitude_name(self, value):
+        self._latitude_name = value
+
+    def reformat_meta(self):
+        """
+        Put both resolution of longitude and latitude at the same resolution. The resolution kept is the biggest one.
+        """
+        # ERA5 has a different format of latitude and longitude because it depends on the resolution
+        longitude25 = self.longitude_name_res(0.25)
+        latitude25 = self.latitude_name_res(0.25)
+        longitude50 = self.longitude_name_res(0.50)
+        latitude50 = self.latitude_name_res(0.50)
+        ds = self.dataset.copy()
+        # Adjust resolution of dimensions latitude025 and longitude025
+        ds[latitude25] = ds[latitude50]
+        ds[longitude25] = ds[longitude50]
+
+        # Adjust resolution of variables variables that depend on latitude025 and longitude025
+        variables_to_adjust = [var_name for var_name in ds.data_vars if
+                               ('latitude025' in ds[var_name].dims) and ('longitude025' in ds[var_name].dims)]
+
+        for var_name in variables_to_adjust:
+            ds[var_name] = ds[var_name].interp(latitude025=ds[latitude25], longitude025=ds[longitude25])
+        self.dataset = ds
+        # New longitude and latitude names are these with the resolution of 050
+        self.longitude_name = longitude50
+        self.latitude_name = latitude50
+
+    @property
+    def unecessary_vars_in_coloc_product(self):
+        """
+        Get unecessary variables in co-location product
+
+        Returns
+        -------
+        list[str]
+            Unecessary variables in co-location product
+        """
+        return [self.time_name, 'land', 'nodata', 'ice', 'cloud']
+
+    @property
+    def necessary_attrs_in_coloc_product(self):
+        """
+        Get necessary dataset attributes in co-location product
+
+        Returns
+        -------
+        list[str]
+            Necessary dataset attributes in co-location product
+        """
+        # No attributes to the original dataset
+        return []
+
+    def rename_attrs_in_coloc_product(self, attr):
+        """
+        Get the new name of an attribute in co-location products from an original attribute
+
+        Parameters
+        ----------
+        attr: str
+            Attribute from the satellite dataset that needs to be renames for the co-location product.
+
+        Returns
+        -------
+        str
+            New attribute's name from the satellite dataset.
+        """
+        # No attributes to the original dataset
+        return ""
